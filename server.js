cbe21d7e8a55ea0d51391ea9341e4d803e684be5
Rodolfo Castillo <brodycastillo77@gmail.com>
2024-11-08 11:36:22 -0600

    Port in env

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require("module-alias/register");
 
+const port = process.env.PORT || 3000;
 const config = require("./src/core/config");
 const fastify = require("fastify")({ logger: true });
 // const nodeEnv = process.env.NODE_ENV || 'development';
@@ -25,7 +26,7 @@ const start = async () => {
         );
         routesFn();
         await fastify.ready();
-        await fastify.listen({ port: 4000, host: "0.0.0.0" });
+        await fastify.listen({ port: port, host: "0.0.0.0" });
     } catch (err) {
         console.log(err);
         process.exit(1);
